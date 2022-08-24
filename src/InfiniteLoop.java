3c9eabc273e6873269d5fb3269f7fb8d5aaf4cda
Kayla Holmes <Kayla.Holmes@cdpipelinedevshop>
2022-08-24 10:10:51 -0400

    "Finished Activities"

---
+++ src/InfiniteLoop.java
@@ -0,0 +1,13 @@
+public class InfiniteLoop {
+    public static void main(String[] args) {
+
+        // The for loop will run infinite times.
+        for(int t =0; t <5; t++){
+            System.out.println("Hello");
+        }
+
+        // To terminate this program press ctrl + c in the console.
+    }
+}
+
+
